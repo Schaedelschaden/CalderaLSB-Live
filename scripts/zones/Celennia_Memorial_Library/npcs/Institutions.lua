caa9ed91ed9596633e8f83bb5a35e76e2d91999e
Barthandalus <Drobinsona149@hotmail.com>
2022-09-17 16:04:21 -0400

    Celennia Memorial Library - Caldera NPC module overrides (#11)
    * This creates a new set of module overrides to apply custom Caldera NPC code to Celennia Memorial Library Zone NPCs. Additionally this creates empty lua files for otherwise nonfunctional NPCs in the LSB Zone folder and also removes functions from DefaultActions.lua to allow for their dialogue to be bypassed.
    
    * Whitespace Adjustment

---
+++ scripts/zones/Celennia_Memorial_Library/npcs/Institutions.lua
@@ -0,0 +1,14 @@
+-----------------------------------
+-- Area: Celennia Memorial Library (284)
+--  NPC: Institutions
+-- 
+-----------------------------------
+local entity = {}
+
+entity.onTrigger = function(player, npc)
+end
+
+entity.onEventFinish = function(player, csid, option)
+end
+
+return entity
